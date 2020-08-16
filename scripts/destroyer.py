69155bd213914aaa6de2d15bc42a9b8399abb833
oleh <alexgnedenko@gmail.com>
2020-08-16 17:41:30 +0300

    add rotating blade to wheels

--- scripts/destroyer.py
+++ scripts/destroyer.py
@@ -5,6 +5,10 @@ class Destroyer(object):
     """docstring"""
     UI_left_wheel_settings_file = "../settings/left_wheel_settings.json"
     UI_right_wheel_settings_file = "../settings/right_wheel_settings.json"
+
+    UI_left_blade_settings_file = "../settings/left_blade_settings.json"
+    UI_right_blade_settings_file = "../settings/right_blade_settings.json"
+
     SKILLS_file = "../settings/SKILLS.json"
 
     def __init__(self):
@@ -12,9 +16,10 @@ class Destroyer(object):
         with open(self.SKILLS_file) as f:
             settings = json.load(f)
         SKILL_rotation_speed = settings["rotation_speed"]
+        SKILL_n_blade = settings["n_blade"]
 
-        self.left_wheel = Wheel(SKILL_rotation_speed, self.UI_left_wheel_settings_file)
-        self.right_wheel = Wheel(SKILL_rotation_speed, self.UI_right_wheel_settings_file)
+        self.left_wheel = Wheel(SKILL_rotation_speed, SKILL_n_blade, self.UI_left_wheel_settings_file, self.UI_left_blade_settings_file)
+        self.right_wheel = Wheel(SKILL_rotation_speed, SKILL_n_blade, self.UI_right_wheel_settings_file, self.UI_right_blade_settings_file)
 
     def update(self, user_activity):
         self.left_wheel.update(user_activity, "left")
--- scripts/wheel.py
+++ scripts/wheel.py
@@ -3,20 +3,48 @@ from picture import *
 class Wheel(object):
     """docstring"""
 
-    def __init__(self, SKILL_rotation_speed, setting_file):
+    def __init__(self, SKILL_rotation_speed, SKILL_n_blade, wheel_setting_file, blade_setting_file):
         """Constructor"""
+        # creating wheel
         self.SKILL_rotation_speed = SKILL_rotation_speed
-        self.UI_wheel = Picture(setting_file)
+        self.UI_wheel = Picture(wheel_setting_file)
+
+        # creating blade
+        self.SKILL_n_blade = SKILL_n_blade
+        self.UI_blade = []
+        for i in range(self.SKILL_n_blade):
+            self.UI_blade.append(Picture(blade_setting_file))
+
+        # set blade angles
+        angle_step = 180//self.SKILL_n_blade
+        for i in range(self.SKILL_n_blade):
+            self.UI_blade[i].angle = i*angle_step
 
     def update(self, user_activity, direction):
         if direction == "right":
+            # rotate wheel
             self.UI_wheel.angle += self.SKILL_rotation_speed
             if self.UI_wheel.angle > 360:
                 self.UI_wheel.angle -= 360
+
+            # rotate blade
+            for blade in self.UI_blade:
+                blade.angle += self.SKILL_rotation_speed
+                if blade.angle > 360:
+                    blade.angle -= 360
         else:
+            # rotate wheel
             self.UI_wheel.angle -= self.SKILL_rotation_speed
             if self.UI_wheel.angle < -360:
                 self.UI_wheel.angle += 360
 
+            # rotate blade
+            for blade in self.UI_blade:
+                blade.angle -= self.SKILL_rotation_speed
+                if blade.angle < -360:
+                    blade.angle += 360
+
     def draw(self, surface):
+        for blade in self.UI_blade:
+            blade.draw(surface)
         self.UI_wheel.draw(surface)
