0f68440eaae0a73addd7e4e5f06737b8cbe09c2a
oleh <alexgnedenko@gmail.com>
2020-08-18 11:22:26 +0300

    created six frames for updates

--- scripts/UI.py
+++ scripts/UI.py
@@ -6,17 +6,35 @@ class UI(object):
     UI_bg_settings_file = "../settings/bg_settings.json"
     UI_main_space_frame_settings_file = "../settings/main_space_frame_settings.json"
     UI_balance_space_frame_settings_file = "../settings/balance_space_frame_settings.json"
-    UI_updates_space_frame_settings_file = "../settings/updates_space_frame_settings.json"
+
+    UI_updates_00_space_frame_settings_file = "../settings/updates_00_space_frame_settings.json"
+    UI_updates_01_space_frame_settings_file = "../settings/updates_01_space_frame_settings.json"
+    UI_updates_02_space_frame_settings_file = "../settings/updates_02_space_frame_settings.json"
+    UI_updates_10_space_frame_settings_file = "../settings/updates_10_space_frame_settings.json"
+    UI_updates_11_space_frame_settings_file = "../settings/updates_11_space_frame_settings.json"
+    UI_updates_12_space_frame_settings_file = "../settings/updates_12_space_frame_settings.json"
 
     def __init__(self):
         """Constructor"""
         self.UI_bg = Picture(self.UI_bg_settings_file)
         self.UI_main_space_frame = Frame(self.UI_main_space_frame_settings_file)
         self.UI_balance_space_frame = Frame(self.UI_balance_space_frame_settings_file)
-        self.UI_updates_space_frame = Frame(self.UI_updates_space_frame_settings_file)
+
+        self.UI_updates_00_space_frame = Frame(self.UI_updates_00_space_frame_settings_file)
+        self.UI_updates_01_space_frame = Frame(self.UI_updates_01_space_frame_settings_file)
+        self.UI_updates_02_space_frame = Frame(self.UI_updates_02_space_frame_settings_file)
+        self.UI_updates_10_space_frame = Frame(self.UI_updates_10_space_frame_settings_file)
+        self.UI_updates_11_space_frame = Frame(self.UI_updates_11_space_frame_settings_file)
+        self.UI_updates_12_space_frame = Frame(self.UI_updates_12_space_frame_settings_file)
 
     def draw(self, surface):
         self.UI_bg.draw(surface)
         self.UI_main_space_frame.draw(surface)
         self.UI_balance_space_frame.draw(surface)
-        self.UI_updates_space_frame.draw(surface)
+
+        self.UI_updates_00_space_frame.draw(surface)
+        self.UI_updates_01_space_frame.draw(surface)
+        self.UI_updates_02_space_frame.draw(surface)
+        self.UI_updates_10_space_frame.draw(surface)
+        self.UI_updates_11_space_frame.draw(surface)
+        self.UI_updates_12_space_frame.draw(surface)
